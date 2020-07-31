be46aaed90267db0b901a9fffeeb6a2e5fa26470
Blank <blank.xu@qq.com>
2020-07-31 10:40:00 +0800

    format codes

--- adapter.go
+++ adapter.go
@@ -1,3 +1,17 @@
+// Copyright 2020 by Blank-Xu. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package sqlxadapter
 
 import (
@@ -28,84 +42,73 @@ type Adapter struct {
 
 	sqlCreateTable   string
 	sqlIsTableExist  string
-	sqlSelectAll     string
+	sqlTruncateTable string
 	sqlInsertRow     string
-	sqlDeleteByArgs  string
 	sqlDeleteAll     string
 	sqlDeleteRow     string
-	sqlTruncateTable string
+	sqlDeleteByArgs  string
+	sqlSelectAll     string
 }
 
-func loadPolicyLine(line *CasbinRule, model model.Model) {
-	var lineBuf bytes.Buffer
-	lineBuf.Grow(32)
-
-	lineBuf.WriteString(line.PType)
-	if line.V0 != "" {
-		lineBuf.WriteByte(',')
-		lineBuf.WriteString(line.V0)
-	}
-	if line.V1 != "" {
-		lineBuf.WriteByte(',')
-		lineBuf.WriteString(line.V1)
-	}
-	if line.V2 != "" {
-		lineBuf.WriteByte(',')
-		lineBuf.WriteString(line.V2)
-	}
-	if line.V3 != "" {
-		lineBuf.WriteByte(',')
-		lineBuf.WriteString(line.V3)
-	}
-	if line.V4 != "" {
-		lineBuf.WriteByte(',')
-		lineBuf.WriteString(line.V4)
-	}
-	if line.V5 != "" {
-		lineBuf.WriteByte(',')
-		lineBuf.WriteString(line.V5)
+func NewAdapter(db *sqlx.DB, tableName string) (*Adapter, error) {
+	if db == nil {
+		return nil, errors.New("db is nil")
 	}
 
-	persist.LoadPolicyLine(lineBuf.String(), model)
+	// check db connecting
+	err := db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	if tableName == "" {
+		tableName = DefaultTableName
+	}
+
+	adapter := &Adapter{
+		db:        db,
+		tableName: tableName,
+	}
+
+	adapter.genSql()
+
+	if !adapter.isTableExist() {
+		if err = adapter.createTable(); err != nil {
+			return nil, err
+		}
+	}
+
+	return adapter, nil
 }
 
-func savePolicyLine(ptype string, rule []string) *CasbinRule {
-	line := CasbinRule{PType: ptype}
-
-	if len(rule) > 0 {
-		line.V0 = rule[0]
-	}
-	if len(rule) > 1 {
-		line.V1 = rule[1]
-	}
-	if len(rule) > 2 {
-		line.V2 = rule[2]
-	}
-	if len(rule) > 3 {
-		line.V3 = rule[3]
-	}
-	if len(rule) > 4 {
-		line.V4 = rule[4]
-	}
-	if len(rule) > 5 {
-		line.V5 = rule[5]
+func (p *Adapter) genSql() {
+	switch p.db.DriverName() {
+	case "postgres", "pgx", "pq-timeouts", "cloudsqlpostgres":
+		p.sqlCreateTable = fmt.Sprintf(sqlCreateTablePostgresql, p.tableName)
+	case "mysql":
+		p.sqlCreateTable = fmt.Sprintf(sqlCreatTableMysql, p.tableName)
+	case "sqlite3":
+		p.sqlCreateTable = fmt.Sprintf(sqlCreateTableSqlite3, p.tableName)
+	case "oci8", "ora", "goracle":
+		p.sqlCreateTable = fmt.Sprintf(sqlCreateTableOracle, p.tableName)
+	case "sqlserver":
+		p.sqlCreateTable = fmt.Sprintf(sqlCreateTableSqlserver, p.tableName)
+	default:
+		p.sqlCreateTable = fmt.Sprintf(sqlCreatTable, p.tableName)
 	}
 
-	return &line
+	p.sqlIsTableExist = fmt.Sprintf(sqlIsTableExist, p.tableName)
+	p.sqlTruncateTable = fmt.Sprintf(sqlTruncateTable, p.tableName)
+	p.sqlInsertRow = fmt.Sprintf(sqlInsertRow, p.tableName)
+	p.sqlDeleteAll = fmt.Sprintf(sqlDeleteAll, p.tableName)
+	p.sqlDeleteRow = fmt.Sprintf(sqlDeleteRow, p.tableName)
+	p.sqlDeleteByArgs = fmt.Sprintf(sqlDeleteByArgs, p.tableName)
+
+	p.sqlSelectAll = fmt.Sprintf(sqlSelectAll, p.tableName)
 }
 
 func (p *Adapter) createTable() error {
 	_, err := p.db.Exec(p.sqlCreateTable)
-	return err
-}
-
-func (p *Adapter) truncateTable() error {
-	_, err := p.db.Exec(p.sqlTruncateTable)
-	return err
-}
-
-func (p *Adapter) deleteAll() error {
-	_, err := p.db.Exec(p.sqlDeleteAll)
 	return err
 }
 
@@ -114,8 +117,59 @@ func (p *Adapter) isTableExist() bool {
 	return err == nil
 }
 
+func (p *Adapter) truncateTable() error {
+	_, err := p.db.Exec(p.sqlTruncateTable)
+	return err
+}
+
 func (p *Adapter) insertRow(line *CasbinRule) error {
 	_, err := p.db.NamedExec(p.sqlInsertRow, line)
+	return err
+}
+
+func (p *Adapter) deleteAll() error {
+	_, err := p.db.Exec(p.sqlDeleteAll)
+	return err
+}
+
+func (p *Adapter) deleteRow(line *CasbinRule) error {
+	_, err := p.db.NamedExec(p.sqlDeleteRow, line)
+	return err
+}
+
+func (p *Adapter) deleteByArgs(line *CasbinRule) error {
+	args := make([]interface{}, 0, 4)
+	args = append(args, line.PType)
+
+	sqlBuf := bytes.NewBufferString(p.sqlDeleteByArgs)
+
+	if line.V0 != "" {
+		sqlBuf.WriteString(" AND `v0` = ?")
+		args = append(args, line.V0)
+	}
+	if line.V1 != "" {
+		sqlBuf.WriteString(" AND `v1` = ?")
+		args = append(args, line.V1)
+	}
+	if line.V2 != "" {
+		sqlBuf.WriteString(" AND `v2` = ?")
+		args = append(args, line.V2)
+	}
+	if line.V3 != "" {
+		sqlBuf.WriteString(" AND `v3` = ?")
+		args = append(args, line.V3)
+	}
+	if line.V4 != "" {
+		sqlBuf.WriteString(" AND `v4` = ?")
+		args = append(args, line.V4)
+	}
+	if line.V5 != "" {
+		sqlBuf.WriteString(" AND `v5` = ?")
+		args = append(args, line.V5)
+	}
+
+	_, err := p.db.Exec(sqlBuf.String(), args...)
+
 	return err
 }
 
@@ -125,7 +179,7 @@ func (p *Adapter) truncateAndInsetRows(lines []*CasbinRule) error {
 		return err
 	}
 
-	if err = p.truncateTable(); err != nil {
+	if _, err = tx.Exec(p.sqlTruncateTable); err != nil {
 		if err1 := tx.Rollback(); err1 != nil {
 			err = fmt.Errorf("truncate err: %v, rollback err: %v", err, err1)
 		}
@@ -144,71 +198,10 @@ func (p *Adapter) truncateAndInsetRows(lines []*CasbinRule) error {
 	return tx.Commit()
 }
 
-func (p *Adapter) deleteRow(line *CasbinRule) error {
-	_, err := p.db.NamedExec(p.sqlDeleteRow, line)
-	return err
-}
-
-func NewAdapter(db *sqlx.DB, tableName string) (*Adapter, error) {
-	if db == nil {
-		return nil, errors.New("db is nil")
-	}
-
-	// check db connected
-	err := db.Ping()
-	if err != nil {
-		return nil, err
-	}
-
-	if tableName == "" {
-		tableName = DefaultTableName
-	}
-
-	adapter := &Adapter{
-		db:        db,
-		tableName: tableName,
-	}
-
-	if err = adapter.genSql(); err != nil {
-		return nil, err
-	}
-
-	if !adapter.isTableExist() {
-		if err = adapter.createTable(); err != nil {
-			return nil, err
-		}
-	}
-
-	return adapter, nil
-}
-
 func (p *Adapter) selectAll() (lines []*CasbinRule, err error) {
 	lines = make([]*CasbinRule, 0, 50)
 	err = p.db.Select(&lines, p.sqlSelectAll)
 	return
-}
-
-func (p *Adapter) genSql() error {
-	switch p.db.DriverName() {
-	case "sqlite3":
-	case "mysql":
-	case "postgresql":
-	case "mssql":
-	case "oracle":
-
-	default:
-		return fmt.Errorf("not support driver name: %s", p.db.DriverName())
-	}
-
-	p.sqlCreateTable = fmt.Sprintf("%s", p.tableName)
-	p.sqlIsTableExist = fmt.Sprintf("SELECT 1 FROM `%s`", p.tableName)
-	p.sqlSelectAll = fmt.Sprintf("SELECT * FROM `%s`;", p.tableName)
-	p.sqlInsertRow = fmt.Sprintf("INSERT INTO `%s` (`p_type`, `v0`, `v1`, `v2`, `v3`, `v4`, `v5`) VALUES (:p_type, :v0, :v1, :v2, :v3, :v4, :v5);", p.tableName)
-	p.sqlDeleteByArgs = fmt.Sprintf("DELETE FROM `%s` WHERE `p_type` = :p_type", p.tableName)
-	p.sqlDeleteRow = fmt.Sprintf("DELETE FROM `%s` WHERE `p_type` = :p_type AND `v0` = :v0 AND `v1` = :v1 AND `v2` = :v2 AND `v3` = :v3 AND `v4` = :v4 AND `v5` = :v5;", p.tableName)
-	p.sqlTruncateTable = fmt.Sprintf("TRUNCATE TABLE `%s`;", p.tableName)
-
-	return nil
 }
 
 func (p *Adapter) LoadPolicy(model model.Model) error {
@@ -280,38 +273,60 @@ func (p *Adapter) RemoveFilteredPolicy(sec string, ptype string, fieldIndex int,
 	return p.deleteRow(&line)
 }
 
-func (p *Adapter) rawDelete(line *CasbinRule) error {
-	args := make([]interface{}, 0, 4)
-	args = append(args, line.PType)
+func loadPolicyLine(line *CasbinRule, model model.Model) {
+	var lineBuf bytes.Buffer
+	lineBuf.Grow(32)
 
-	sqlBuf := bytes.NewBufferString(p.sqlDeleteByArgs)
-
+	lineBuf.WriteString(line.PType)
 	if line.V0 != "" {
-		sqlBuf.WriteString(" AND `v0` = ?")
-		args = append(args, line.V0)
+		lineBuf.WriteByte(',')
+		lineBuf.WriteString(line.V0)
 	}
 	if line.V1 != "" {
-		sqlBuf.WriteString(" AND `v1` = ?")
-		args = append(args, line.V1)
+		lineBuf.WriteByte(',')
+		lineBuf.WriteString(line.V1)
 	}
 	if line.V2 != "" {
-		sqlBuf.WriteString(" AND `v2` = ?")
-		args = append(args, line.V2)
+		lineBuf.WriteByte(',')
+		lineBuf.WriteString(line.V2)
 	}
 	if line.V3 != "" {
-		sqlBuf.WriteString(" AND `v3` = ?")
-		args = append(args, line.V3)
+		lineBuf.WriteByte(',')
+		lineBuf.WriteString(line.V3)
 	}
 	if line.V4 != "" {
-		sqlBuf.WriteString(" AND `v4` = ?")
-		args = append(args, line.V4)
+		lineBuf.WriteByte(',')
+		lineBuf.WriteString(line.V4)
 	}
 	if line.V5 != "" {
-		sqlBuf.WriteString(" AND `v5` = ?")
-		args = append(args, line.V5)
+		lineBuf.WriteByte(',')
+		lineBuf.WriteString(line.V5)
 	}
 
-	_, err := p.db.Exec(sqlBuf.String(), args...)
+	persist.LoadPolicyLine(lineBuf.String(), model)
+}
 
-	return err
+func savePolicyLine(ptype string, rule []string) *CasbinRule {
+	line := CasbinRule{PType: ptype}
+
+	if len(rule) > 0 {
+		line.V0 = rule[0]
+	}
+	if len(rule) > 1 {
+		line.V1 = rule[1]
+	}
+	if len(rule) > 2 {
+		line.V2 = rule[2]
+	}
+	if len(rule) > 3 {
+		line.V3 = rule[3]
+	}
+	if len(rule) > 4 {
+		line.V4 = rule[4]
+	}
+	if len(rule) > 5 {
+		line.V5 = rule[5]
+	}
+
+	return &line
 }
--- sql.go
+++ sql.go
@@ -1,26 +1,125 @@
+// Copyright 2020 by Blank-Xu. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package sqlxadapter
 
-// for sqlite3
+// general sql
 const (
-	sqlCreateSqlite3 = ""
+	sqlCreatTable = `
+CREATE TABLE %s
+(
+    p_type varchar(32),
+    v0     varchar(255),
+    v1     varchar(255),
+    v2     varchar(255),
+    v3     varchar(255),
+    v4     varchar(255),
+    v5     varchar(255),
+    INDEX idx_casbin_rule_ptype (p_type, v0, v1)
+);
+`
+	sqlIsTableExist  = "SELECT 1 FROM `%s`;"
+	sqlTruncateTable = "TRUNCATE TABLE `%s`;"
+	sqlInsertRow     = "INSERT INTO `%s` (`p_type`, `v0`, `v1`, `v2`, `v3`, `v4`, `v5`) VALUES (:p_type, :v0, :v1, :v2, :v3, :v4, :v5);"
+	sqlDeleteAll     = "DELETE FROM `%s`;"
+	sqlDeleteRow     = "DELETE FROM `%s` WHERE `p_type` = :p_type AND `v0` = :v0 AND `v1` = :v1 AND `v2` = :v2 AND `v3` = :v3 AND `v4` = :v4 AND `v5` = :v5;"
+	sqlDeleteByArgs  = "DELETE FROM `%s` WHERE `p_type` = :p_type"
+	sqlSelectAll     = "SELECT * FROM `%s`;"
 )
 
-// for mysql
+// for Sqlite3
 const (
-	sqlCreateMysql = ""
+	sqlCreateTableSqlite3 = `
+CREATE TABLE %s
+(
+    p_type varchar(32),
+    v0     varchar(255),
+    v1     varchar(255),
+    v2     varchar(255),
+    v3     varchar(255),
+    v4     varchar(255),
+    v5     varchar(255),
+    INDEX idx_casbin_rule_ptype (p_type, v0, v1)
+);
+`
 )
 
-// for postgresql
+// for Mysql
 const (
-	sqlCreatePostgresql = ""
+	sqlCreatTableMysql = `
+CREATE TABLE %s
+(
+    p_type varchar(32)  NOT NULL DEFAULT '',
+    v0     varchar(255) NOT NULL DEFAULT '',
+    v1     varchar(255) NOT NULL DEFAULT '',
+    v2     varchar(255) NOT NULL DEFAULT '',
+    v3     varchar(255) NOT NULL DEFAULT '',
+    v4     varchar(255) NOT NULL DEFAULT '',
+    v5     varchar(255) NOT NULL DEFAULT '',
+    INDEX idx_casbin_rule_ptype (p_type, v0, v1)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+`
 )
 
-// for mssql
+// for Postgresql
 const (
-	sqlCreateMssql = ""
+	sqlCreateTablePostgresql = `
+CREATE TABLE %s
+(
+    p_type varchar(32)  NOT NULL DEFAULT '',
+    v0     varchar(255) NOT NULL DEFAULT '',
+    v1     varchar(255) NOT NULL DEFAULT '',
+    v2     varchar(255) NOT NULL DEFAULT '',
+    v3     varchar(255) NOT NULL DEFAULT '',
+    v4     varchar(255) NOT NULL DEFAULT '',
+    v5     varchar(255) NOT NULL DEFAULT '',
+    INDEX idx_casbin_rule_ptype (p_type, v0, v1)
+);
+`
 )
 
-// for oracle
+// for Sqlserver
 const (
-	sqlCreateOracle = ""
+	sqlCreateTableSqlserver = `
+CREATE TABLE %s
+(
+    p_type nvarchar(32)  NOT NULL DEFAULT '',
+    v0     nvarchar(255) NOT NULL DEFAULT '',
+    v1     nvarchar(255) NOT NULL DEFAULT '',
+    v2     nvarchar(255) NOT NULL DEFAULT '',
+    v3     nvarchar(255) NOT NULL DEFAULT '',
+    v4     nvarchar(255) NOT NULL DEFAULT '',
+    v5     nvarchar(255) NOT NULL DEFAULT '',
+    INDEX idx_casbin_rule_ptype (p_type, v0, v1)
+);
+`
+)
+
+// for Oracle
+const (
+	sqlCreateTableOracle = `
+CREATE TABLE %s
+(
+    p_type NVARCHAR2(32),
+    v0     NVARCHAR2(255),
+    v1     NVARCHAR2(255),
+    v2     NVARCHAR2(255),
+    v3     NVARCHAR2(255),
+    v4     NVARCHAR2(255),
+    v5     NVARCHAR2(255),
+    INDEX idx_casbin_rule_ptype (p_type, v0, v1)
+);
+`
 )
