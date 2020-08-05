3cd69163a865fab36aa3039114fe4dd63a69aded
Blank <blank.xu@qq.com>
2020-08-05 08:20:30 +0800

    update test coverage

--- adapter_test.go
+++ adapter_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package tests
+package sqlxadapter
 
 import (
 	"log"
@@ -23,28 +23,26 @@ import (
 	"github.com/casbin/casbin/v2/util"
 	"github.com/jmoiron/sqlx"
 
-	sqlxadapter "github.com/Blank-Xu/sqlx-adapter"
-
 	_ "github.com/denisenkom/go-mssqldb"
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-var (
-	rbacModelFile  = "../examples/rbac_model.conf"
-	rbacPolicyFile = "../examples/rbac_policy.csv"
+const (
+	rbacModelFile  = "examples/rbac_model.conf"
+	rbacPolicyFile = "examples/rbac_policy.csv"
+)
 
+var (
 	dataSourceNames = map[string]string{
 		"sqlite3":  ":memory:",
 		"mysql":    "root:@tcp(127.0.0.1:3306)/sqlx_adapter_test",
 		"postgres": "user=postgres host=127.0.0.1 port=5432 dbname=sqlx_adapter_test sslmode=disable",
 		// "sqlserver": "sqlserver://sa:YourPassword@127.0.0.1:1433?database=sqlx_adapter_test&connection+timeout=30",
 	}
-)
 
-var (
-	lines = []sqlxadapter.CasbinRule{
+	lines = []CasbinRule{
 		{PType: "p", V0: "alice", V1: "data1", V2: "read"},
 		{PType: "p", V0: "bob", V1: "data2", V2: "read"},
 		{PType: "p", V0: "bob", V1: "data2", V2: "write"},
@@ -56,7 +54,7 @@ var (
 		{PType: "g", V0: "bob", V1: "data1_admin", V2: "test2", V3: "test3", V4: "test4", V5: "test5"},
 	}
 
-	filter = sqlxadapter.Filter{
+	filter = Filter{
 		PType: []string{"p"},
 		V0:    []string{"bob", "data2_admin"},
 		V1:    []string{"data1", "data2"},
@@ -101,60 +99,56 @@ func TestAdapters(t *testing.T) {
 }
 
 func testTableName(t *testing.T, db *sqlx.DB) {
-	_, err := sqlxadapter.NewAdapter(db, "")
+	_, err := NewAdapter(db, "")
 	if err != nil {
-		t.Fatalf("sqlxadapter.NewAdapter failed, err: %v", err)
+		t.Fatalf("NewAdapter failed, err: %v", err)
 	}
 }
 
 func testSql(t *testing.T, db *sqlx.DB, tableName string) {
-	a, err := sqlxadapter.NewAdapter(db, tableName)
+	a, err := NewAdapter(db, tableName)
 	if err != nil {
-		t.Fatalf("sqlxadapter.NewAdapter failed, err: %v", err)
-	}
-
-	test := sqlxadapter.Test{
-		Adapter: a,
+		t.Fatalf("NewAdapter failed, err: %v", err)
 	}
 
 	// CreateTable test has passed when adapter create
-	// if err = test.CreateTable(); err != nil {
+	// if err = a.CreateTable(); err != nil {
 	// 	t.Fatal("CreateTable failed, err: ", err)
 	// }
 
-	if b := test.IsTableExist(); b == false {
+	if b := a.isTableExist(); b == false {
 		t.Fatal("IsTableExist test failed")
 	}
 
-	if err = test.InsertRow(lines[0]); err != nil {
+	if err = a.insertRow(lines[0]); err != nil {
 		t.Fatal("InsertRow test failed, err: ", err)
 	}
 
-	if err = test.TruncateAndInsertRows(lines); err != nil {
+	if err = a.truncateAndInsertRows(lines); err != nil {
 		t.Fatal("TruncateAndInsertRows test failed, err: ", err)
 	}
 
-	if err = test.DeleteAll(); err != nil {
+	if err = a.deleteAll(); err != nil {
 		t.Fatal("DeleteAll test failed, err: ", err)
 	}
 
-	if err = test.TruncateAndInsertRows(lines); err != nil {
+	if err = a.truncateAndInsertRows(lines); err != nil {
 		t.Fatal("TruncateAndInsertRows test failed, err: ", err)
 	}
 
-	if err = test.DeleteByArgs(sqlxadapter.CasbinRule{}); err != nil && err.Error() != "invalid delete args" {
+	if err = a.deleteByArgs(CasbinRule{}); err != nil && err.Error() != "invalid delete args" {
 		t.Fatal("DeleteByArgs test without args failed, err: ", err)
 	}
 
-	if err = test.DeleteByArgs(lines[8]); err != nil {
+	if err = a.deleteByArgs(lines[8]); err != nil {
 		t.Fatal("DeleteByArgs test failed, err: ", err)
 	}
 
-	if err = test.TruncateAndInsertRows(lines); err != nil {
+	if err = a.truncateAndInsertRows(lines); err != nil {
 		t.Fatal("TruncateAndInsertRows test failed, err: ", err)
 	}
 
-	records, err := test.SelectAll()
+	records, err := a.selectAll()
 	if err != nil {
 		t.Fatal("SelectAll test failed, err: ", err)
 	}
@@ -171,7 +165,7 @@ func testSql(t *testing.T, db *sqlx.DB, tableName string) {
 		}
 	}
 
-	records, err = test.SelectWhereIn(filter)
+	records, err = a.selectWhereIn(filter)
 	if err != nil {
 		t.Fatal("SelectWhereIn test failed, err: ", err)
 	}
@@ -192,22 +186,18 @@ func testSql(t *testing.T, db *sqlx.DB, tableName string) {
 		i += 1
 	}
 
-	if err = test.TruncateTable(); err != nil {
+	if err = a.truncateTable(); err != nil {
 		t.Fatal("TruncateTable test failed, err: ", err)
 	}
 }
 
 func testPolicyLine(t *testing.T, db *sqlx.DB, tableName string) {
-	a, err := sqlxadapter.NewAdapter(db, tableName)
+	a, err := NewAdapter(db, tableName)
 	if err != nil {
-		t.Fatalf("sqlxadapter.NewAdapter failed, err: %v", err)
+		t.Fatalf("NewAdapter failed, err: %v", err)
 	}
 
-	test := sqlxadapter.Test{
-		Adapter: a,
-	}
-
-	testLine := sqlxadapter.CasbinRule{
+	testLine := CasbinRule{
 		PType: "p",
 		V0:    "test0",
 		V1:    "test1",
@@ -218,7 +208,7 @@ func testPolicyLine(t *testing.T, db *sqlx.DB, tableName string) {
 	}
 	rule := []string{"test0", "test1", "test2", "test3", "test4", "test5"}
 
-	line := test.GenPolicyLine("p", rule)
+	line := a.genPolicyLine("p", rule)
 
 	if testLine.PType != line.PType ||
 		testLine.V0 != line.V0 ||
@@ -231,7 +221,7 @@ func testPolicyLine(t *testing.T, db *sqlx.DB, tableName string) {
 	}
 
 	e, _ := casbin.NewEnforcer(rbacModelFile, rbacPolicyFile)
-	test.LoadPolicyLine(&line, e.GetModel())
+	a.loadPolicyLine(&line, e.GetModel())
 }
 
 func initPolicy(t *testing.T, db *sqlx.DB, tableName string) {
@@ -239,9 +229,9 @@ func initPolicy(t *testing.T, db *sqlx.DB, tableName string) {
 	// so we need to load the policy from the file adapter (.CSV) first.
 	e, _ := casbin.NewEnforcer(rbacModelFile, rbacPolicyFile)
 
-	a, err := sqlxadapter.NewAdapter(db, tableName)
+	a, err := NewAdapter(db, tableName)
 	if err != nil {
-		t.Fatal("sqlxadapter.NewAdapter test failed, err: ", err)
+		t.Fatal("NewAdapter test failed, err: ", err)
 	}
 
 	// This is a trick to save the current policy to the DB.
@@ -273,7 +263,7 @@ func testSaveLoad(t *testing.T, db *sqlx.DB, tableName string) {
 	// Now the DB has policy, so we can provide a normal use case.
 	// Create an adapter and an enforcer.
 	// NewEnforcer() will load the policy automatically.
-	a, _ := sqlxadapter.NewAdapter(db, tableName)
+	a, _ := NewAdapter(db, tableName)
 	e, _ := casbin.NewEnforcer(rbacModelFile, a)
 	testGetPolicy(t, e, [][]string{{"alice", "data1", "read"}, {"bob", "data2", "write"}, {"data2_admin", "data2", "read"}, {"data2_admin", "data2", "write"}})
 }
@@ -287,7 +277,7 @@ func testAutoSave(t *testing.T, db *sqlx.DB, tableName string) {
 	// Now the DB has policy, so we can provide a normal use case.
 	// Create an adapter and an enforcer.
 	// NewEnforcer() will load the policy automatically.
-	a, _ := sqlxadapter.NewAdapter(db, tableName)
+	a, _ := NewAdapter(db, tableName)
 	e, _ := casbin.NewEnforcer(rbacModelFile, a)
 
 	// AutoSave is enabled by default.
@@ -334,25 +324,25 @@ func testFilteredPolicy(t *testing.T, db *sqlx.DB, tableName string) {
 	// Now the DB has policy, so we can provide a normal use case.
 	// Create an adapter and an enforcer.
 	// NewEnforcer() will load the policy automatically.
-	a, _ := sqlxadapter.NewAdapter(db, tableName)
+	a, _ := NewAdapter(db, tableName)
 	e, _ := casbin.NewEnforcer(rbacModelFile, a)
 	// Now set the adapter
 	e.SetAdapter(a)
 
 	// Load only alice's policies
-	e.LoadFilteredPolicy(sqlxadapter.Filter{V0: []string{"alice"}})
+	e.LoadFilteredPolicy(Filter{V0: []string{"alice"}})
 	testGetPolicy(t, e, [][]string{{"alice", "data1", "read"}})
 
 	// Load only bob's policies
-	e.LoadFilteredPolicy(sqlxadapter.Filter{V0: []string{"bob"}})
+	e.LoadFilteredPolicy(Filter{V0: []string{"bob"}})
 	testGetPolicy(t, e, [][]string{{"bob", "data2", "write"}})
 
 	// Load policies for data2_admin
-	e.LoadFilteredPolicy(sqlxadapter.Filter{V0: []string{"data2_admin"}})
+	e.LoadFilteredPolicy(Filter{V0: []string{"data2_admin"}})
 	testGetPolicy(t, e, [][]string{{"data2_admin", "data2", "read"}, {"data2_admin", "data2", "write"}})
 
 	// Load policies for alice and bob
-	e.LoadFilteredPolicy(sqlxadapter.Filter{V0: []string{"alice", "bob"}})
+	e.LoadFilteredPolicy(Filter{V0: []string{"alice", "bob"}})
 	testGetPolicy(t, e, [][]string{{"alice", "data1", "read"}, {"bob", "data2", "write"}})
 
 	e.AddPolicy("bob", "data1", "write", "test1", "test2", "test3")
